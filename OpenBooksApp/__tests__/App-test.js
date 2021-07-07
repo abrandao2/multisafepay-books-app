f27de76e7012dd171bf8d4ceaf482d38cb8ab22d
Andre Silva <a.silva@tools4brokers.com>
2021-07-07 14:32:25 +0300

    Write a very simple test

--- OpenBooksApp/__tests__/App-test.js
+++ OpenBooksApp/__tests__/App-test.js
@@ -5,10 +5,25 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
+import {render, fireEvent} from '@testing-library/react-native';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+describe('App works properly', () => {
+  const {getByPlaceholderText, getByText} = render(<App />);
+
+  it('renders correctly', () => {
+    renderer.create(<App />);
+  });
+
+  it('should render searching notice while typing', () => {
+    const searchInput = getByPlaceholderText('Search books...');
+
+    fireEvent.changeText(searchInput, 'abcdefg');
+
+    const searchingNotice = getByText('Searching...');
+
+    expect(searchingNotice).not.toBeNull();
+  });
 });
