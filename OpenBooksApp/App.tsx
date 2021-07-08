87dbc82d4e67cad1213c4247d6b6047cde556833
Andre Silva <a.silva@tools4brokers.com>
2021-07-09 00:01:53 +0300

    Add redux store and store provider

--- OpenBooksApp/App.tsx
+++ OpenBooksApp/App.tsx
@@ -1,15 +1,20 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {Button} from 'react-native';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import Search from './components/Search';
 import BookDetails from './components/BookDetails';
+import rootReducer from './redux/rootReducer';
 
 import Colors from './const/colors';
 
 const Stack = createStackNavigator();
+const store = createStore(rootReducer, applyMiddleware(thunk));;
 
 const basicHeaderOptions = {
   headerStyle: {
@@ -20,32 +25,34 @@ const basicHeaderOptions = {
 
 const App: React.FC = (): JSX.Element => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Search">
-        <Stack.Screen
-          name="Search"
-          component={Search}
-          options={{
-            ...basicHeaderOptions,
-            title: 'Open Books Search',
-          }}
-        />
-        <Stack.Screen
-          name="BookDetails"
-          component={BookDetails}
-          options={({navigation}) => ({
-            ...basicHeaderOptions,
-            title: 'Book Details',
-            headerLeft: () => (
-              <Button
-                title="Back"
-                onPress={() => navigation.navigate('Search')}
-              />
-            ),
-          })}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Search">
+          <Stack.Screen
+            name="Search"
+            component={Search}
+            options={{
+              ...basicHeaderOptions,
+              title: 'Open Books Search',
+            }}
+          />
+          <Stack.Screen
+            name="BookDetails"
+            component={BookDetails}
+            options={({navigation}) => ({
+              ...basicHeaderOptions,
+              title: 'Book Details',
+              headerLeft: () => (
+                <Button
+                  title="Back"
+                  onPress={() => navigation.navigate('Search')}
+                />
+              ),
+            })}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 };
 
